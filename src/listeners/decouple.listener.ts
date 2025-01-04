e3349610192b9e4960674ffb7d42c69c6809b004
Islam Yamor <islamyamorz@gmail.com>
2025-01-05 01:02:06 +0300

    fixed sender string

--- src/listeners/decouple.listener.ts
+++ src/listeners/decouple.listener.ts
@@ -3,6 +3,7 @@ import { message } from 'telegraf/filters'
 import { env } from '../service/validate-env'
 import ProcessedLink from '../db/links.schema'
 import { sendNotification } from '../functions/send-notification'
+import { concat } from 'concat-str'
 
 export const decoupleListener = () => {
   return bot.on(message('text'), async (ctx) => {
@@ -17,7 +18,7 @@ export const decoupleListener = () => {
       [
         '@invitelinkscleaner:',
         'New incoming message',
-        `sender: ${ctx.from.first_name + ctx.from?.last_name}`,
+        `sender: ${concat(ctx.from.first_name, ctx.from?.last_name ?? '')}`,
         `username: ${username}`,
         `${messageText}`,
       ].join('\n')
