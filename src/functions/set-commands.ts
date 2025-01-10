ae03b6101801db76a10e2a2163826eed0abb9f48
Islam Yamor <islamyamorz@gmail.com>
2025-01-10 23:09:00 +0300

    removed the login command

--- src/functions/set-commands.ts
+++ src/functions/set-commands.ts
@@ -4,6 +4,5 @@ export const setCommands = () => {
   bot.telegram.setMyCommands([
     { command: 'start', description: 'Start the bot' },
     { command: 'help', description: 'Get help about the bot' },
-    { command: 'login', description: 'To enable deleting expired links from the chat' },
   ])
 }
